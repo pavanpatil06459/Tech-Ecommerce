0f87d1c328b1f0ec11040bc97e763467c7cf23fe
pavanpatil06459 <pavanpatil06459@gmail.com>
2026-03-22 02:33:16 +0530

    update

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,12 +3,9 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const token = sessionStorage.getItem("authToken");
-
   if (!token) {
-    // If no token in this tab’s sessionStorage, redirect to login
     return <Navigate to="/login" replace />;
   }
-
   return children;
 };
 
